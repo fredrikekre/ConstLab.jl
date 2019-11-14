2df5b252845febcc3b1dcfea52e55aac147d8bb4
Fredrik Ekre <fredrik.ekre@chalmers.se>
2019-11-14 16:36:55 +0100

    jfdksljdksl

--- src/ConstLab.jl
+++ src/ConstLab.jl
@@ -1,6 +1,7 @@
 module ConstLab
 
-using Tensors, Parameters, ForwardDiff
+using Tensors, Parameters
+import NLsolve, ForwardDiff
 
 # Stress and strain control
 abstract type ControlType end
@@ -59,30 +60,37 @@ end
 
 mise(s) = √(3/2) * norm(dev(s))
 
-function constitutive_driver(model::Plastic, ε::SymmetricTensor, state::PlasticState)
+function constitutive_driver(model::Plastic, ε::SymmetricTensor, state::PlasticState;
+        solver_params=Dict{Symbol,Any}())
     G, K, σ_y = model.G, model.K, model.σ_y
 
     # Assume elastic step
     Δε = ε - state.ε
     σ_tr_dev = dev(state.σ + 2G * Δε)
-    @show mise(σ_tr_dev - state.α)
+    # @show mise(σ_tr_dev - state.α)
     Φ = mise(σ_tr_dev - state.α) - σ_y - state.κ
     # @show Φ
     if Φ < 0 # elastic
-        @info "elastic"
+        # @info "elastic step" Φ
         σ_dev = σ_tr_dev # + K * tr(ε) * one(σ_tr_dev)
         κ = state.κ
         α = state.α
         μ = state.μ
     else
-        @info "plastic"
+        # @info "plastic step; solving local problem" Φ
         # need to iterate
-        σ_dev, κ, α, μ = solve_local_problem(model, Δε, state)
+        σ_dev, κ, α, μ = solve_local_problem(model, Δε, state; solver_params=solver_params)
         # σ = σ_dev + K * tr(ε) * one(σ_tr_dev)
         # @show mise(σ_dev - α) - σ_y - κ
     end
     σ = σ_dev + K * tr(ε) * one(σ_dev)
     return PlasticState(ε, σ, κ, α, μ)
+end
+
+function pack_variables(σ, κ, α, μ::T) where T
+    n = length(σ) + length(κ) + length(α) + length(μ)
+    X = Vector{T}(undef, n)
+    return pack_variables!(X, σ, κ, α, μ)
 end
 
 function pack_variables!(X, σ, κ, α, μ)
@@ -92,6 +100,7 @@ function pack_variables!(X, σ, κ, α, μ)
     X[end] = μ
     return X
 end
+
 function extract_variables(X)
     N = 6
     σ = fromvoigt(SymmetricTensor{2,3}, X)
@@ -101,7 +110,7 @@ function extract_variables(X)
     return σ, κ, α, μ
 end
 
-function solve_local_problem(model::Plastic, Δε::SymmetricTensor, state::PlasticState{D,N}) where {D,N}
+function solve_local_problem(model::Plastic, Δε::SymmetricTensor, state::PlasticState{D,N}; solver_params=Dict{Symbol,Any}()) where {D,N}
     # extract material paremeters
     @unpack G, K, σ_y, κ∞, α∞, r, H = model
 
@@ -109,19 +118,14 @@ function solve_local_problem(model::Plastic, Δε::SymmetricTensor, state::Plast
     κₙ = state.κ
     αₙ = state.α
     μₙ = state.μ * 0 # ??
-
-    # σₙ_dev = dev(σₙ - αₙ)
     σ_tr_dev = dev(σₙ) + 2G * dev(Δε)
 
-    # pack variables
-    X = zeros(14)
-    # ΔX = zeros(14)
-    R = zeros(14)
-    pack_variables!(X, σ_tr_dev, κₙ, αₙ, μₙ)
+    # Initial guess
+    X0 = pack_variables(σ_tr_dev, κₙ, αₙ, μₙ)
 
     function residual!(R, X)
-        fill!(R, 0)
-        # unpack variables
+        # fill!(R, 0) # pack_variables! should overwrite
+        # Unpack variables
         σ, κ, α, μ = extract_variables(X)
         σ_red_dev = dev(σ - α)
 
@@ -131,34 +135,42 @@ function solve_local_problem(model::Plastic, Δε::SymmetricTensor, state::Plast
         Rα = dev(α) - dev(αₙ) - (1-r) * H * μ * (σ_red_dev / mise(σ_red_dev) - 1/α∞ * dev(α))
         RΦ = mise(σ_red_dev) - σ_y - κ
 
-        # populate R
+        # Populate residual vector R
         pack_variables!(R, Rσ, Rκ, Rα, RΦ)
         return R
     end
 
-    cfg = ForwardDiff.JacobianConfig(residual!, R, X)
-    out = DiffResults.JacobianResult(X)
+    # Use NLsolve to solve non-linear system
+    params = Dict{Symbol,Any}(:autodiff=>:forward, :ftol=>1e-6, :show_trace=>false)
+    merge!(params, solver_params)
+    res = NLsolve.nlsolve(residual!, X0; params...)
+    X = res.zero
 
-    iters = 0
-    while true; iters += 1
-        # residual!(R, X)
-        ForwardDiff.jacobian!(out, residual!, R, X, cfg)
-        g = DiffResults.value(out)
-        J = DiffResults.jacobian(out)
-        if norm(g, Inf) < 1e-6
-            @info "local problem converged" iters norm(g)
-            break
-        elseif iters > 20
-            error("did not converge")
-        end
-        ΔX = J \ g
-        X .-= ΔX
-    end
+    # # Custom implementation of Newton's algorithm
+    # X = X0 # zeros(14)
+    # R = zeros(14)
+    # cfg = ForwardDiff.JacobianConfig(residual!, R, X)
+    # out = DiffResults.JacobianResult(X)
+    # iters = 0
+    # while true; iters += 1
+    #     # residual!(R, X)
+    #     ForwardDiff.jacobian!(out, residual!, R, X, cfg)
+    #     g = DiffResults.value(out)
+    #     J = DiffResults.jacobian(out)
+    #     if norm(g) < 1e-6
+    #         @info "local problem converged" iters norm(g)
+    #         break
+    #     elseif iters > 20
+    #         error("did not converge")
+    #     end
+    #     ΔX = J \ g
+    #     X .-= ΔX
+    # end
+
     σ, κ, α, μ = extract_variables(X)
-    @info "local problem converged" dev(σ) κ dev(α) μ
+
     return dev(σ), κ, dev(α), μ
 end
-
 
 
 ###############################################
@@ -171,12 +183,12 @@ end
 
 Integrate the material response.
 """
-function integrate(model::Material, ctrl::ControlType, time, state::T) where {T <: MaterialState}
+function integrate(model::Material, ctrl::ControlType, time, state::T;
+        solver_params=Dict{Symbol,Any}()) where {T <: MaterialState}
     states = T[]
     for (i, t) in enumerate(time)
         ε = ctrl.f(t)
-        @show ε
-        state′ = constitutive_driver(model, ε, state)
+        state′ = constitutive_driver(model, ε, state; solver_params=solver_params)
         push!(states, state′)
         state = state′
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ import ConstLab
 using ConstLab: LinearElastic, LinearElasticState, StrainControl,
       Plastic, PlasticState
 
-σ_amp = 0.005
+σ_amp = 0.004
 ctrl = StrainControl((t) -> SymmetricTensor{2,3}((i,j) -> i == j == 1 ? σ_amp*sin(t) : 0.0))
 time = range(0.0, 2pi; length=100)
 
@@ -21,10 +21,10 @@ E = 200.0e9
 G = E/(2*(1+ν))
 K = E/(3*(1-2*ν))
 σ_y = 200.0e6
-H = 0.1*E
+H = 0.1*E*0
 κ∞ = 0.2*σ_y
 α∞ = 0.1*σ_y
-r = 0.5
+r = 0.5*0
 # σ_c = 0.1*E
 # t_star = 100.0
 # n = 1.5
@@ -42,16 +42,8 @@ using PGFPlotsX, LaTeXStrings
 
 push!(empty!(PGFPlotsX.CUSTOM_PREAMBLE), raw"\usepackage{amsmath}")
 
-mise(s) = √(3/2) * norm(dev(s))
-
 p = @pgf Axis({xlabel=L"\varepsilon_{11}", ylabel=L"\sigma_\mathrm{e}^\mathrm{vM}"},
-    PlotInc({},
-        Coordinates([r.ε[1,1] for r in res],
-                    [mise(r.σ-r.α) for r in res])
-    ),
-)
-p = @pgf Axis({xlabel=L"\varepsilon_{11}", ylabel=L"\sigma_\mathrm{e}^\mathrm{vM}"},
-    PlotInc({},
+    PlotInc({mark="none"},
         Coordinates([r.ε[1,1] for r in res],
                     [r.σ[1,1] for r in res])
     ),
