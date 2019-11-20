22d1febbed836ef47e2bba18cb5dca4628de0179
Fredrik Ekre <fredrik.ekre@chalmers.se>
2019-11-20 13:23:15 +0100

    Compute ATS tensor

--- src/ConstLab.jl
+++ src/ConstLab.jl
@@ -58,6 +58,18 @@ struct PlasticState{D,N} <: MaterialState
     μ::Float64
 end
 
+function elastic_tangent(model::Plastic)
+    G, K = model.G, model.K
+    # Use Lamé parameters
+    λ = K - 2G/3
+    μ = G
+    δ(i,j) = i == j ? 1.0 : 0.0
+    E = SymmetricTensor{4,3}() do i, j, k, l
+        λ * δ(i,j) * δ(k,l) + μ * (δ(i,k) * δ(j,l) + δ(i,l) * δ(j,k))
+    end
+    return E
+end
+
 mise(s) = √(3/2) * norm(dev(s))
 
 function constitutive_driver(model::Plastic, ε::SymmetricTensor, state::PlasticState;
@@ -72,19 +84,20 @@ function constitutive_driver(model::Plastic, ε::SymmetricTensor, state::Plastic
     # @show Φ
     if Φ < 0 # elastic
         # @info "elastic step" Φ
-        σ_dev = σ_tr_dev # + K * tr(ε) * one(σ_tr_dev)
+        σ_dev = σ_tr_dev
         κ = state.κ
         α = state.α
         μ = state.μ
+        dσdε = elastic_tangent(model)
     else
         # @info "plastic step; solving local problem" Φ
         # need to iterate
-        σ_dev, κ, α, μ = solve_local_problem(model, Δε, state; solver_params=solver_params)
+        σ_dev, κ, α, μ, dσdε = solve_local_problem(model, Δε, state; solver_params=solver_params)
         # σ = σ_dev + K * tr(ε) * one(σ_tr_dev)
         # @show mise(σ_dev - α) - σ_y - κ
     end
     σ = σ_dev + K * tr(ε) * one(σ_dev)
-    return PlasticState(ε, σ, κ, α, μ)
+    return σ, dσdε, PlasticState(ε, σ, κ, α, μ)
 end
 
 function pack_variables(σ, κ, α, μ::T) where T
@@ -111,7 +124,7 @@ function extract_variables(X)
 end
 
 function solve_local_problem(model::Plastic, Δε::SymmetricTensor, state::PlasticState{D,N}; solver_params=Dict{Symbol,Any}()) where {D,N}
-    # extract material paremeters
+    # extract material parameters
     @unpack G, K, σ_y, κ∞, α∞, r, H = model
 
     σₙ = state.σ
@@ -123,6 +136,7 @@ function solve_local_problem(model::Plastic, Δε::SymmetricTensor, state::Plast
     # Initial guess
     X0 = pack_variables(σ_tr_dev, κₙ, αₙ, μₙ)
 
+    # Residual function
     function residual!(R, X)
         # fill!(R, 0) # pack_variables! should overwrite
         # Unpack variables
@@ -144,7 +158,18 @@ function solve_local_problem(model::Plastic, Δε::SymmetricTensor, state::Plast
     params = Dict{Symbol,Any}(:autodiff=>:forward, :ftol=>1e-6, :show_trace=>false)
     merge!(params, solver_params)
     res = NLsolve.nlsolve(residual!, X0; params...)
+    NLsolve.converged(res) || error("old local problem did not converge: $res")
     X = res.zero
+
+    # Use NLsolve to solve non-linear system
+    params = Dict{Symbol,Any}(:ftol=>1e-6, :show_trace=>false)
+    merge!(params, solver_params)
+    R = similar(X0) # residual cache for constructing OnceDifferentiable (not used :( )
+    cache = NLsolve.OnceDifferentiable(residual!, X0, R; autodiff=:forward)
+    res = NLsolve.nlsolve(cache, X0; params...)
+    NLsolve.converged(res) || error("local problem did not converge: $res")
+    X = res.zero # Solution to the problem
+    J = cache.DF # Extract Jacobian from last step
 
     # # Custom implementation of Newton's algorithm
     # X = X0 # zeros(14)
@@ -152,6 +177,7 @@ function solve_local_problem(model::Plastic, Δε::SymmetricTensor, state::Plast
     # cfg = ForwardDiff.JacobianConfig(residual!, R, X)
     # out = DiffResults.JacobianResult(X)
     # iters = 0
+    # local J
     # while true; iters += 1
     #     # residual!(R, X)
     #     ForwardDiff.jacobian!(out, residual!, R, X, cfg)
@@ -167,9 +193,14 @@ function solve_local_problem(model::Plastic, Δε::SymmetricTensor, state::Plast
     #     X .-= ΔX
     # end
 
+    # Extract variables from the solution X
     σ, κ, α, μ = extract_variables(X)
+    # Extract ATS tensor from final Jacobian
+    dRdε = elastic_tangent(model) # For fixed X
+    Jt = fromvoigt(Tensor{4,3}, J)
+    dσdε = inv(Jt) ⊡ dRdε
 
-    return dev(σ), κ, dev(α), μ
+    return dev(σ), κ, dev(α), μ, dσdε
 end
 
 
@@ -188,7 +219,7 @@ function integrate(model::Material, ctrl::ControlType, time, state::T;
     states = T[]
     for (i, t) in enumerate(time)
         ε = ctrl.f(t)
-        state′ = constitutive_driver(model, ε, state; solver_params=solver_params)
+        σ, dσdε, state′ = constitutive_driver(model, ε, state; solver_params=solver_params)
         push!(states, state′)
         state = state′
     end
